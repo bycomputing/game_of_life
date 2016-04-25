accb9c58e08e39ea334975fb9dd32026107c59c4
Rye Salvador <salvadorrye@gmail.com>
2016-04-25 15:04:19 +0800

    Initialize repository

---
+++ src/com/wordpress/bycomputing/Life.java
@@ -0,0 +1,102 @@
+package com.wordpress.bycomputing;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import com.wordpress.bycomputing.LinkedList.Linkable;
+
+@SuppressWarnings("serial")
+public class Life extends JFrame implements ActionListener {	
+	
+	public Life() {
+		super();
+		this.setTitle("Game of Life");
+		this.setSize(640, 480);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);		
+			
+		String[] buttons = {"Start", "Clear", "Gen", "Speed", "Quit",
+				            "Number of generations #100"};
+				
+		JPanel mainPanel = new JPanel(new BorderLayout());						
+		JPanel buttonsPanel = new JPanel();				
+		
+		for (int i = 0; i < buttons.length; i++) {
+			if (i == buttons.length -1) {
+				JLabel label = new JLabel(buttons[i]);
+				buttonsPanel.add(label);
+			} else {
+			    JButton button = new JButton(buttons[i]);
+		        button.addActionListener(this);
+		        buttonsPanel.add(button);
+			}
+		}
+		mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+		add(mainPanel);		
+	}
+
+	static class LinkableCell implements Linkable
+	{
+		int x, y;
+		Linkable next;
+		public LinkableCell(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public Linkable getNext() {
+			return next;
+		}
+		@Override
+		public void setNext(Linkable node) {
+			next = node;				
+		}
+		
+		public String toString() {
+			return x + " " + y;
+		}
+		
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (!(o instanceof LinkableCell)) return false;
+			if (((LinkableCell) o).x == this.x && ((LinkableCell) o).y == this.y) return true;
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			Life life = new Life();
+            life.setVisible(true);
+		});		                  
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "Start":
+			System.out.println("Starting...");
+			break;
+		case "Clear":
+			System.out.println("Clearing...");
+			break;
+		case "Gen":
+			System.out.println("Changing number of generations...");
+			break;
+		case "Speed":
+			System.out.println("Changing speed...");
+			break;
+		case "Quit":
+			System.exit(0);
+		default:
+			break;
+		};		
+	}
+}
---
+++ src/com/wordpress/bycomputing/LinkedList.java
@@ -0,0 +1,68 @@
+package com.wordpress.bycomputing;
+
+public class LinkedList {
+	Linkable head;
+	
+    public interface Linkable {
+    	public Linkable getNext();
+    	public void setNext(Linkable node);
+    }
+
+	public synchronized Linkable getHead() {
+		return head;
+	}
+    
+	public synchronized void insertAtHead(Linkable node) {
+		node.setNext(head);
+		head = node;
+	}
+	
+    public synchronized void insertAtTail(Linkable node) {
+    	if (head == null) head = node;
+    	else {
+    		Linkable p, q;
+    		for (p = head; (q = p.getNext()) != null; p = q);
+    		p.setNext(node);
+    	}
+    }
+    
+    public synchronized Linkable removeFromHead() {
+    	Linkable node = head;
+    	if (node != null) {
+    		head = node.getNext();
+    		node.setNext(null);
+    	}
+    	return node;
+    }
+    
+    public synchronized Linkable removeFromTail() {
+    	if (head == null) return null;
+    	Linkable p = head, q = null, next = head.getNext();
+    	if (next == null) {
+    		head = null;
+    		return p;
+    	}
+    	while ((next = p.getNext()) != null) {
+    		q = p;
+    		p = next;
+    	}
+    	q.setNext(null);
+    	return p;
+    }
+    
+    public synchronized void remove(Linkable node) {
+    	if (head == null) return;
+    	if (node.equals(head)) {
+    		head = head.getNext();
+    		return;
+    	}
+    	Linkable p = head, q = null;
+    	while ((q = p.getNext()) != null) {
+    		if (node.equals(q)) {
+    			p.setNext(q.getNext());
+    			return;
+    		}
+    		p = q;
+    	}
+    }    
+}
