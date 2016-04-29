379170bddd060448a81f4d35be1b540e5b2324da
Rye Salvador <salvadorrye@gmail.com>
2016-04-29 12:24:29 +0800

    Simulate game

--- src/com/wordpress/bycomputing/DrawPanel.java
+++ src/com/wordpress/bycomputing/DrawPanel.java
@@ -7,31 +7,32 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
-
 import javax.swing.JPanel;
+import com.wordpress.bycomputing.Life.LinkableCell;
 
 @SuppressWarnings("serial")
-public class DrawPanel extends JPanel implements MouseListener, MouseMotionListener {
+public class DrawPanel extends JPanel implements MouseListener, MouseMotionListener, Runnable {
 	
-	private static final int MAXCOL = 50, MAXROW = 28, ALIVE = 1, DEAD = 0;
-	private int row, col;
-	private static int[][] world, nums;
+	private static final int MAXCOL = 50, MAXROW = 28;
+	private int gens = 0;
 	private static Graphics2D g2d;
-			
-	public DrawPanel() {
-		world = new int[MAXROW][MAXCOL];
-		row = col = -1;
+	private Life game;
+	LinkedList cells;			
+	
+	public DrawPanel(Life game) {
+		this.game = game;
+		cells = new LinkedList();
 		addMouseListener(this);
-		addMouseMotionListener(this);
+		addMouseMotionListener(this);		
 	}
 	
 	public void clearWorld() {
-		world = new int[MAXROW][MAXCOL];
+		cells = new LinkedList();
 		repaint();		
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) { addPoint(e); }	
+	public void mouseClicked(MouseEvent e) { addLinkableCell(e.getX(), e.getY()); }	
 
 	@Override
 	public void mouseEntered(MouseEvent e) {}
@@ -43,10 +44,10 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
 	public void mousePressed(MouseEvent e) {}
 
 	@Override
-	public void mouseReleased(MouseEvent e) { addPoint(e); }
+	public void mouseReleased(MouseEvent e) { addLinkableCell(e.getX(), e.getY()); }
 	
 	@Override
-	public void mouseDragged(MouseEvent e) { addPoint(e); }
+	public void mouseDragged(MouseEvent e) { addLinkableCell(e.getX(), e.getY()); }
 
 	@Override
 	public void mouseMoved(MouseEvent e) {}
@@ -59,32 +60,77 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
 
 	private void doDrawing(Graphics g) {		
 		g2d = (Graphics2D) g;		
-		g2d.setColor(Color.BLUE);
+		g2d.setColor(Color.DARK_GRAY);
 		
 		for (int y = 55; y < 400; y += 12)
 		    g2d.drawLine(20, y, getWidth() - 10, y);
 		for (int x = 20; x < 630; x += 12)
 			g2d.drawLine(x, 55, x, 391);
 		
-		for (int r = 0; r < MAXROW; ++r)
-			for (int c = 0; c < MAXCOL; ++c)
-				if (world[r][c] == ALIVE) {
-					g2d.setPaint(Color.RED);
-					g2d.draw(new Ellipse2D.Double(c * 12 + 23, r * 12 + 57, 7, 7));
-					g2d.setPaint(Color.GREEN);
-			        g2d.fill(new Ellipse2D.Double(c * 12 + 23, r * 12 + 57, 7, 7));
-		        }
+		for (LinkableCell l = (LinkableCell) cells.getHead(); l != null; l = (LinkableCell) l.getNext()) {			
+			g2d.setPaint(Color.RED);			
+			g2d.draw(new Ellipse2D.Double(
+					l.getX() * 12 + 23,l.getY() * 12 + 57, 7, 7));			
+			g2d.setPaint(Color.GREEN);			
+	        g2d.fill(new Ellipse2D.Double(
+	        		l.getX() * 12 + 23, l.getY() * 12 + 57, 7, 7));
+		}
 	}
 	
-	private void addPoint(MouseEvent e) {
-		if ((e.getX() > 20) && (e.getX() < 620) &&
-				(e.getY() > 56) && (e.getY() < 390)) {			
-				col = (e.getX() - 20) / 12;
-				row = (e.getY() - 56) / 12;
-				if (world[row][col] == DEAD) {				
-					world[row][col] = ALIVE;
-					repaint();
-				}
+	private void addLinkableCell(int x, int y) {
+		if ((x > 20) && (x < 620) && (y > 56) && (y < 390)) {			
+				int col = (x - 20) / 12;
+				int row = (y - 56) / 12;
+				cells.remove(new LinkableCell(col, row));
+				cells.insertAtHead(new LinkableCell(col, row));
+				repaint();
 			}		
-	}	
+	}
+
+	@Override
+	public void run() {
+		if (gens < game.getGenerations()) {
+			simulate();
+			++gens;
+			game.label.setText("Generations #" + gens);
+			try {
+				Thread.sleep(1000/game.getSpeed());
+				run();
+			} catch (Exception e) {}
+		}
+		gens = 0;		
+	}
+
+	private void simulate() {
+        boolean[][] world = new boolean[MAXCOL+2][MAXROW+2];		
+		for (LinkableCell l = (LinkableCell) cells.getHead(); l != null; l = (LinkableCell) l.getNext()) {
+			world[l.getX()+1][l.getY()+1] = true;			
+		}
+		
+		LinkedList survivors = new LinkedList();
+		for (int i = 1; i < world.length - 1; i++)
+			for (int j = 1; j < world[0].length - 1; j++) {
+				int neighbors = 0;
+				if (world[i-1][j-1]) { neighbors++; }
+                if (world[i-1][j])   { neighbors++; }
+                if (world[i-1][j+1]) { neighbors++; }
+                if (world[i][j-1])   { neighbors++; }
+                if (world[i][j+1])   { neighbors++; }
+                if (world[i+1][j-1]) { neighbors++; }
+                if (world[i+1][j])   { neighbors++; }
+                if (world[i+1][j+1]) { neighbors++; }
+                if (world[i][j]) {                     
+                    if ((neighbors == 2) || (neighbors == 3)) 
+                        survivors.insertAtHead(new LinkableCell(i-1,j-1));                    
+                } else {
+                    if (neighbors == 3)                     	
+                        survivors.insertAtHead(new LinkableCell(i-1,j-1));                    
+                }
+			}
+		cells = new LinkedList();
+		for (LinkableCell l = (LinkableCell) survivors.getHead(); l != null; l = (LinkableCell) l.getNext()) {
+			cells.insertAtHead(new LinkableCell(l.getX(), l.getY()));
+		}
+		repaint();		
+	}			
 }
--- src/com/wordpress/bycomputing/Life.java
+++ src/com/wordpress/bycomputing/Life.java
@@ -14,25 +14,26 @@ import com.wordpress.bycomputing.LinkedList.Linkable;
 @SuppressWarnings("serial")
 public class Life extends JFrame implements ActionListener {
 	
-	private int speed = 1, generations = 100;
-	private final String[] BUTTONS = { "Start", "Clear","Gens",
-			                           "Speed", "Quit",
-			                           "Generation #" + generations };
+	private int speed = 1, gens = 100;
+	private final String[] BUTTONS = { "Start", "Clear",
+			"Gens", "Speed", "Quit", "Generation #" + gens };
+	private Thread game;
 	JLabel label;
-	DrawPanel drawPanel;
-	SliderDialogs sliderDialogs;
+	DrawPanel gameBoard;
+	SliderDialogs sliderDialogs;	
 
 	public Life() {
 		super();
-		setTitle("Game of Life");
-		setSize(640, 480);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);		
+		this.setTitle("Game of Life");
+		this.setSize(640, 480);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);		
 				
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		JPanel buttonsPanel = new JPanel();
-		drawPanel = new DrawPanel();
+		gameBoard = new DrawPanel(this);
+		
 		sliderDialogs = new SliderDialogs(this);
 		
 		for (int i = 0; i < BUTTONS.length; i++) {
@@ -45,45 +46,41 @@ public class Life extends JFrame implements ActionListener {
 		        buttonsPanel.add(button);
 			}
 		}
-		
-		mainPanel.add(drawPanel);
+		mainPanel.add(gameBoard);
 		mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
-		add(mainPanel);		
+		this.add(mainPanel);		
 	}
 	
-	public int getSpeed() {
-		return speed;
-	}
+	public int getSpeed() { return speed; }
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
+	public void setSpeed(int speed) { this.speed = speed; }
 
-	public int getGenerations() {
-		return generations;
-	}
+	public int getGenerations() { return gens; }
 
-	public void setGenerations(int generations) {
-		this.generations = generations;
-	}
+	public void setGenerations(int gens) { this.gens = gens; }
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-		case "Start":			
+		case "Start":
+			game = new Thread(gameBoard);
+			game.start();
 			break;
 		case "Clear":
-			drawPanel.clearWorld();
+			gameBoard.clearWorld();
 			break;
 		case "Gens":
-			JOptionPane.showMessageDialog(null, sliderDialogs.newGensDialog());
+			JOptionPane.showMessageDialog(
+					null, sliderDialogs.newGensDialog());
 			break;
 		case "Speed":
-			JOptionPane.showMessageDialog(null, sliderDialogs.newSpeedDialog());
+			JOptionPane.showMessageDialog(
+					null, sliderDialogs.newSpeedDialog());
 			break;
 		case "Quit":
 			int response = JOptionPane.showConfirmDialog(
-					this, "Do you really want to quit?", "Message", JOptionPane.YES_NO_OPTION);
+					this, "Do you really want to quit?",
+					"Message", JOptionPane.YES_NO_OPTION);
 			switch (response) {
 			case JOptionPane.YES_OPTION:
 				System.exit(0);
@@ -98,43 +95,31 @@ public class Life extends JFrame implements ActionListener {
 	}
 	
 	static class LinkableCell implements Linkable
-	{
-		
+	{		
 		int x, y;
 		Linkable next;
 		
-		public LinkableCell(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+		public LinkableCell(int x, int y) { this.x = x; this.y = y; }
 		
-		public int getX() {
-			return x;
-		}
-
-		public int getY() {
-			return y;
-		}
+		public int getX() { return x; }
 		
-		public String toString() {
-			return x + " " + y;
-		}
+		public int getY() { return y; }
+		
+		public String toString() { return x + " " + y; }
 		
 		public boolean equals(Object o) {
 			if (this == o) return true;
 			if (!(o instanceof LinkableCell)) return false;
-			if (((LinkableCell) o).x == this.x && ((LinkableCell) o).y == this.y) return true;
+			if (((LinkableCell) o).x == this.x
+					&& ((LinkableCell) o).y == this.y) return true;
 			return false;
 		}
 		
 		@Override
-		public Linkable getNext() {
-			return next;
-		}
+		public Linkable getNext() { return next; }
+		
 		@Override
-		public void setNext(Linkable node) {
-			next = node;				
-		}		
+		public void setNext(Linkable node) { next = node; }		
 	}
 	
 	public static void main(String[] args) {
